c3b1efe990987f1a589f81bad527583c9191f671
AbdullahS-21 <abdullahshaikh225@gmail.com>
2021-05-20 17:23:51 +0530

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+
+(function(){
+
+    function Start()
+    {
+        let myVariable = 15;
+        console.log("my Number is: " + myVariable);
+    }
+
+    window.addEventListener("load", Start);
+})();
